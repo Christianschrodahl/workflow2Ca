af7f89aad797e4d1801af08e9df79538ea4efbe8
Christian Schrodahl <christian.schrodahl@gmail.com>
2020-02-23 09:33:49 +0100

    comitting files

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const {
+	src,
+	dest
+} = require('gulp');
+const less = require('gulp-less');
+const minifyCSS = require('gulp-csso');
+const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin')
+
+function css() {
+	return src('less/style.less')
+		.pipe(less())
+		.pipe(minifyCSS())
+		.pipe(dest('dist/css'))
+		.pipe(browserSync.stream())
+
+}
+
+function minifyImage() {
+	return src('images/*')
+		.pipe(imagemin({
+			progressive: true
+		}))
+		.pipe(gulp.dest('dist/images'))
+}
+
+function watch() {
+	browserSync.init({
+		server: {
+			baseDir: './',
+		}
+	});
+	gulp.watch('./less/**/*.less', css);
+	gulp.watch('./*.html').on('change', browserSync.reload)
+}
+
+exports.task = minifyImage;
+exports.watch = watch;
