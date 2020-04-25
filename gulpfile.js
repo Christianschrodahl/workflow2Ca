08b41ccbf1b6bfaf1b8daf67d5960e7740e298fc
Christian Schrodahl <christian.schrodahl@gmail.com>
2020-04-25 13:08:53 +0200

    creating workflow2 project

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,37 @@
-const gulp = require('gulp');
-const {
-	src,
-	dest
-} = require('gulp');
-const less = require('gulp-less');
-const minifyCSS = require('gulp-csso');
-const browserSync = require('browser-sync').create();
-const imagemin = require('gulp-imagemin')
+const gulp = require("gulp");
+const { src, dest } = require("gulp");
+const less = require("gulp-less");
+const minifyCSS = require("gulp-csso");
+const browserSync = require("browser-sync").create();
+const imagemin = require("gulp-imagemin");
 
 function css() {
-	return src('less/style.less')
-		.pipe(less())
-		.pipe(minifyCSS())
-		.pipe(dest('dist/css'))
-		.pipe(browserSync.stream())
-
+  return src("less/style.less")
+    .pipe(less())
+    .pipe(minifyCSS())
+    .pipe(dest("dist/css"))
+    .pipe(browserSync.stream());
 }
 
 function minifyImage() {
-	return src('images/*')
-		.pipe(imagemin({
-			progressive: true
-		}))
-		.pipe(gulp.dest('dist/images'))
+  return src("images/*")
+    .pipe(
+      imagemin({
+        progressive: true,
+      })
+    )
+    .pipe(gulp.dest("dist/images"));
 }
 
 function watch() {
-	browserSync.init({
-		server: {
-			baseDir: './',
-		}
-	});
-	gulp.watch('./less/**/*.less', css);
-	gulp.watch('./*.html').on('change', browserSync.reload)
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  gulp.watch("./less/**/*.less", css);
+  gulp.watch("./images/*", minifyImage);
+  gulp.watch("./*.html").on("change", browserSync.reload);
 }
 
-exports.task = minifyImage;
 exports.watch = watch;
